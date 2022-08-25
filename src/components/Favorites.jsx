9609b78f10c71bd31415f0f0e582a1f5dbfe761c
RogneyCDC <rogneycdc@gmail.com>
2022-08-25 17:12:41 -0500

    actualizando diseño

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,7 +1,9 @@
-import { Grid } from '@mui/material';
+import { Box, CardMedia, Grid } from '@mui/material';
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { PersonCard } from './PersonCard';
+
+import notFavorite from '../img/notFavorite.png';
 
 export default function Favorites() {
   const { peoplesList, favoritesList } = useSelector((state) => state.favorite);
@@ -15,14 +17,27 @@ export default function Favorites() {
   }, [favoritesList]);
 
   return (
-    <>
-      <Grid container spacing={4}>
-        {favoritesList.map((item, index) => (
-          <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
-            <PersonCard person={item} />
-          </Grid>
-        ))}
-      </Grid>
-    </>
+    <Box sx={{ textAlign: 'center' }}>
+      {favoritesList.length === 0 ? (
+        <img
+          style={{
+            maxWidth: '100%',
+            maxHeight: 'calc(100vh - 122px)',
+            height: 'auto',
+            padding: 0,
+            margin: 0,
+          }}
+          src={notFavorite}
+        />
+      ) : (
+        <Grid container spacing={4}>
+          {favoritesList.map((item, index) => (
+            <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+              <PersonCard person={item} />
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    </Box>
   );
 }
